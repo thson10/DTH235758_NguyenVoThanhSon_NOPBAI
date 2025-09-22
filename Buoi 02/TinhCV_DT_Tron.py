d620b12fa82435b9454c669cf5b3eb3dcbc2cfc2
DTH235758_Nguyễn Võ Thanh Sơn <nvthanhson0801@gmail.com>
2025-09-22 19:55:36 +0700

    Delete Buoi 02 directory

--- Buoi 02/TinhCV_DT_Tron.py
+++
@@ -1,7 +0,0 @@
-import math
-
-r = float(input(" Nhap ban kinh hinh tron: "))
-CV=2*r*math.pi
-print(float(CV))
-DT=math.pi*pow(r,2)
-print(float(DT))
--- Buoi 02/TinhGioPhutGiay.py
+++
@@ -1,7 +0,0 @@
-t=(float(input("Nhap so giay: ")))
-gio= t/3600
-phut=t/60
-giay = t
-print(gio)
-print(phut)
-print(giay)
